21b4908070348c878e3673174f246fcf831d0aec
Pranay Srivastava <pranay.srivastava.17.1@mountblue.tech>
2021-10-13 12:58:28 +0530

    First Commit: Project Repo created

---
+++ src/main/java/com/mountblue/StackOverFlow/StackOverFlowApplication.java
@@ -0,0 +1,13 @@
+package com.mountblue.StackOverFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackOverFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackOverFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mountblue/StackOverFlow/StackOverFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mountblue.StackOverFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackOverFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
