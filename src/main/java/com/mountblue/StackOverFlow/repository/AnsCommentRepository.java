7365ced2def119d3364e945870975fdde0d1cd55
AdityaKumar <aditya.kumar.17.1@mountblue.tech>
2021-10-17 13:20:31 +0530

    Added CRUD operation for answer comment

---
+++ src/main/java/com/mountblue/StackOverFlow/repository/AnsCommentRepository.java
@@ -0,0 +1,7 @@
+package com.mountblue.StackOverFlow.repository;
+
+import com.mountblue.StackOverFlow.model.AnsComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnsCommentRepository extends JpaRepository<AnsComment, Integer> {
+}
