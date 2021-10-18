2ef97e4125557ed920fb4a0f7f9199b7a529bf80
Pranay Srivastava <pranay.srivastava.17.1@mountblue.tech>
2021-10-18 23:36:16 +0530

    Made changes in web security config

--- src/main/java/com/mountblue/StackOverFlow/security/WebSecurityConfig.java
+++ src/main/java/com/mountblue/StackOverFlow/security/WebSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -45,11 +46,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/login")
                 .usernameParameter("email")
-                .permitAll();
+                .permitAll()
+                .and()
+                .logout()
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login")
+                .permitAll();;
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/main.css","/registration");
+        web.ignoring().antMatchers("/main.css","/registration","/users","/questions");
     }
 }
