9a63f3c7f1dfda2e4bb00acad68af81fc6a646ca
AdityaKumar <aditya.kumar.17.1@mountblue.tech>
2021-10-18 15:58:11 +0530

    fixed registration page issue

--- src/main/java/com/mountblue/StackOverFlow/controller/LogInRegistrationController.java
+++ src/main/java/com/mountblue/StackOverFlow/controller/LogInRegistrationController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LogInRegistrationController {
 
-    @GetMapping("/registration")
+    /*@GetMapping("/registration")
     public String createNewUser() {
         return "registration";
-    }
-    @GetMapping("/login2")
+    }*/
+    @GetMapping("/login")
     public String login() {
         return "login";
     }
