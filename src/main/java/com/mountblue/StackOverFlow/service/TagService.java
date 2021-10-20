68922463b40d7359782d56329a61b953f3f3c361
Vinayak kamati <vinayakkamati123@gmail.com>
2021-10-20 23:24:17 +0530

    finding the all tags

---
+++ src/main/java/com/mountblue/StackOverFlow/service/TagService.java
@@ -0,0 +1,10 @@
+package com.mountblue.StackOverFlow.service;
+
+import com.mountblue.StackOverFlow.model.Tag;
+
+import java.util.List;
+
+public interface TagService {
+
+    List<Tag> getAllTags();
+}
---
+++ src/main/java/com/mountblue/StackOverFlow/service/impl/TagServiceImpl.java
@@ -0,0 +1,20 @@
+package com.mountblue.StackOverFlow.service.impl;
+
+import com.mountblue.StackOverFlow.model.Tag;
+import com.mountblue.StackOverFlow.repository.TagRepository;
+import com.mountblue.StackOverFlow.service.TagService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TagServiceImpl implements TagService {
+
+    @Autowired
+    private TagRepository tagRepository;
+    @Override
+    public List<Tag> getAllTags() {
+        return tagRepository.findAll();
+    }
+}
