347b05e3074cbca973bb715b30ddd780ef949848
Vinayak kamati <vinayakkamati123@gmail.com>
2021-10-22 02:35:52 +0530

    query for searching user by keyword

--- src/main/java/com/mountblue/StackOverFlow/repository/UserRepository.java
+++ src/main/java/com/mountblue/StackOverFlow/repository/UserRepository.java
@@ -5,10 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
   @Query("SELECT u FROM User  u WHERE u.email = :email")
   public User getUserByEmail(@Param("email")String email);
 
   @Query("SELECT u FROM User u WHERE u.email = ?1")
   User findByEmail(String username);
+
+  @Query("SELECT q FROM User q WHERE q.name LIKE %?1% ")
+  List<User> findAll(String keyword);
 }
